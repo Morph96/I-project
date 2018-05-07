e5f5a16c5ba5a411278c29843c0f91eca51c0048
BBrendeke <bartbren1998@gmail.com>
2018-05-07 12:14:50 +0200

    Html detail pag

---
+++ DetailPagHtml.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Antaris Fifty 5</title>
+</head>
+<body>
+<?php include 'header.php' ?>
+<div class="my-4 d-flex justify-content-around">
+    <div class="flex-wrap col-lg-6">
+        <img class="mb-2 rounded" src="https://www.lenano.nl/wp-content/uploads/2016/06/Boot-schoonmaken-met-Lenano.jpg" alt="Boot">
+        <div class="mb-2 border p-2">
+            <h4>Antaris Fifty 5</h4>
+            <h5>Contactgegevens:</h5>
+            <p>Telefoonnummer: 0655725590<br>
+                Naam: Bart Brendeke<br>
+                Mail: Bartbren1998@gmail.com</p>
+        </div>
+        <div class="border p-2">
+            <h5>Samenvatting:</h5>
+            <p>Super mooie Antaris Fifty 5 met een 33 PK Vetus diesel (4-cil.), Teakline vloer (kunststof) met zwarte bies, Teakline geïntrigeerd op zwemplateau, Koellade ingebouwd voorin in rvs frame, Zwaluwnesten
+                met teakhouten afwerking, Grotere brandstoftank (55 L.), Rvs handgreep op stuurconsole, Rvs zwemtrap geïntrigeerd in zwemplateau, Rvs beschermstrip rondom het zwemplateau, Rvs handgreep op achterspiegel, RVS bekerhouders (dubbel) aan console, Rvs bekerhouders in opstap (4x), JBL Stereo Bluetooth en 4 speakers, Meerprijs afwijkende rompkleur, Teak/RVS stuurwiel 60 cm., Navigatieverlichting (rood/groen in de boeg) en heklicht, Speciale stoffering kussens, Hoes voor de buiskap, Led kuipverlichting, 12 volt aansluiting/USB, Losse klokken op het stuurconsole Prijs v.a. 33.500,00! Direct kontakt opnemen voor een mooie aanbieding? *Eventueel een ligplaats in onze Jachthaven in Amsterdam beschikbaar.
+
+                Antaris Fifty 5
+
+            </p>
+        </div>
+    </div>
+    <div class="border flex-wrap col-lg-2 h-50 p-2">
+        <h5>Biedingen</h5>
+        <p class="my-0">€33.500,00</p>
+        <p class="my-0">€33.700,00</p>
+        <p class="my-0">€33.800,00</p>
+        <p class="my-0">€33.950,00</p>
+    </div>
+</div>
+
+
+
+<?php include 'footer.php' ?>
+</body>
+</html>
